4133af29887501ba7568839c38718878648ce78b
Abdelrahman Soliman <abdelrahman.lifestyle@gmail.com>
2021-08-29 20:51:11 +0200

    render_to_string

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+from django.template.loader import render_to_string
 
 monthly_challenges = {
     "january": "ICE BUCKET CHALLENGE",
@@ -47,7 +48,7 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        resposnse_data = f"<h1>{challenge_text}</h1>"
+        resposnse_data = render_to_string("challenges/challenge.html")
         return HttpResponse(resposnse_data)
     except:
         return HttpResponseNotFound('<h1>This Month is not supported!!!!!</h1>')
