1df48b7fd989aec4eb48775d5a79040da1c2007a
Abdelrahman Soliman <abdelrahman.lifestyle@gmail.com>
2021-08-29 14:53:21 +0200

    add name to route and use reverse

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 monthly_challenges = {
     "january": "ICE BUCKET CHALLENGE",
@@ -26,7 +26,8 @@ def monthly_challenge_by_number(request, month):
         return HttpResponseNotFound("invalid month!")
 
     redirect_month = months[month-1]
-    return HttpResponseRedirect('/challenges/'+redirect_month)
+    redirect_path = reverse("month-challenge", args=[redirect_month])
+    return HttpResponseRedirect(redirect_path)
 
 
 def monthly_challenge(request, month):
