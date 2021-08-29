907de5a9c248af619f05675137235e1ff9adb22d
Abdelrahman Soliman <abdelrahman.lifestyle@gmail.com>
2021-08-29 20:36:52 +0200

    adding index to views

--- challenges/views.py
+++ challenges/views.py
@@ -19,6 +19,20 @@ monthly_challenges = {
 # Create your views here.
 
 
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        capitalized__month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{capitalized__month}</a></li>"
+
+    response_data = f"<ul>{list_items}</ul>"
+
+    return HttpResponse(response_data)
+
+
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
 
